1af4eef4c0854d0577be37f8b0f7363d543c49e1
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-07 18:30:03 -0500

    webpack config ewady

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  stats: {
+    colors: true,
+    modules: true,
+    reasons: true,
+    errorDetails: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    htmlWebpackPlugin
+  ]
+}
+module.exports = config
